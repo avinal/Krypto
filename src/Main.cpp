a34ed2316e5e43fd583e321b139191d876305be2
avinal <185067@nith.ac.in>
2021-02-18 22:09:51 +0530

    main file added

---
+++ src/Main.cpp
@@ -0,0 +1,15 @@
+/**
+ * /mnt/z/my_git/RCRS-CSD-327/src/Main.cpp
+ * @file Main.cpp
+ * @author Avinal Kumar
+ * @since February 18, 2021
+ *
+ * The driver code
+ */
+
+#include <blowfish/blowfish.hpp>
+#include <iostream>
+int main(int argc, char const *argv[]) {
+  std::cout << "Hello Encryption";
+  return 0;
+}
